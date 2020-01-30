51776762c2deca5d318aa54d55c8bbd0d0e5427a
textrivers <jon.p.woodward@gmail.com>
2020-01-30 12:36:36 -0500

    parallax background, new level

---
+++ Scripts/Sun.gd
@@ -0,0 +1,16 @@
+extends Sprite
+
+var direction
+
+const ROT_CONST = 0.05
+
+# Called when the node enters the scene tree for the first time.
+func _ready():
+	randomize()
+	direction = randi() % 2
+	if direction == 0:
+		direction = -1
+
+func _process(delta):
+	$Aura1.rotation_degrees += (ROT_CONST * direction)
+	$Aura2.rotation_degrees -= (ROT_CONST * direction)
