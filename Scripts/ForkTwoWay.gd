772524706908334dffdd41ab5a7460a99486d183
textrivers <jon.p.woodward@gmail.com>
2020-03-03 14:10:22 -0500

    adding two way fork and left fork
    Also fixing transparency in some snow sprites

---
+++ Scripts/ForkTwoWay.gd
@@ -0,0 +1,36 @@
+extends Area2D
+
+var parent_collider
+var ready_to_switch = false
+var switch_up = true
+
+# Called when the node enters the scene tree for the first time.
+func _ready():
+	parent_collider = get_parent().get_node("CollisionPolygon2D")
+	print(parent_collider)
+
+func _process(_delta):
+	if ready_to_switch == true:
+		if Input.is_action_just_pressed("ui_fork_down"):
+			if switch_up == true:
+				parent_collider.disabled = true
+				$SpriteSign/SpriteUpArrow.hide()
+				$SpriteSign/SpriteDownArrow.show()
+				switch_up = false
+			else: 
+				parent_collider.disabled = false
+				$SpriteSign/SpriteUpArrow.show()
+				$SpriteSign/SpriteDownArrow.hide()
+				switch_up = true
+
+func _on_Fork_area_entered(area):
+	if area.is_in_group("player"):
+		ready_to_switch = true
+		$SpriteSign/SpriteForkText.show()
+		$SpriteCtrl.show()
+
+func _on_Fork_area_exited(area):
+	if area.is_in_group("player"):
+		ready_to_switch = false
+		$SpriteSign/SpriteForkText.hide()
+		$SpriteCtrl.hide()
